0a3bf5a87be86e489968f1ff30926e2627cfa6f9
nvFelipe <feneves.fn@gmail.com>
2024-06-14 16:20:23 -0300

    Atualizando estudos e pastas
    Signed-off-by: nvFelipe <feneves.fn@gmail.com>

--- material/src/functional_interface/examples/SupplierExample.java
+++ material/src/functional_interface/examples/SupplierExample.java
@@ -8,13 +8,14 @@ import java.util.stream.Stream;
 public class SupplierExample {
     public static void main(String[] args) {
         //Usar o Supplier com expressao lambda para fornecer uma saudação personalizada
-        Supplier<String> saudacao= () -> "lá, seja bem-vindo";
+        Supplier<String> saudacao= () -> "Olá, seja bem-vindo";
 
         //Usar o Supplier para obter uma lista com 5 saudações
         List<String> listaSaudacoes = Stream.generate(saudacao)
                 .limit(5)
-                .collect(Collectors.toList());
-        listaSaudacoes.forEach(s -> System.out.println(s));
+                .collect(Collectors.toList()); /*Este collecto, a propria IDE que alterar para um toLis
+                mas é aplicado desta maneira*/
+        listaSaudacoes.forEach(s -> System.out.println(s));/*Aqui a IDE tambem quer alterar para method reference*/
     }
 
 }
